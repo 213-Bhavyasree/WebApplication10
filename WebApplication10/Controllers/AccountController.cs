88fc300c0efab08e711f2a2783c0fe07664572b6
Admin <kloklokesh846@gmail.com>
2022-01-03 09:39:55 +0530

    Add project files.

---
+++ WebApplication10/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Account> accounts = new List<Account>();
+            accounts.Add(new Account() { ID = 100, AccHolderName = "Bhavya", AccHolderAddress = "Kadapa"});
+            accounts.Add(new Account() { ID = 200, AccHolderName = "Navya", AccHolderAddress = "Kurnool" });
+            accounts.Add(new Account() { ID = 300, AccHolderName = "Kavya", AccHolderAddress = "Nellore" });
+
+            ViewData["accounts"] = accounts;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication10/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication10.Models
+{
+    public class Account
+    {
+        public int ID { get; set; }
+        public string AccHolderName { get; set; }
+        public string AccHolderAddress { get; set; }
+           
+    }
+}
